0d402563529fd340a98b3da0f006fcd2df259608
AlahmadiQ8 <m91.alahmadi@gmail.com>
2017-08-20 16:00:27 -0700

    misc enhancements

--- stories/index.js
+++ stories/index.js
@@ -7,7 +7,6 @@ import { linkTo } from '@storybook/addon-links';
 import { Button, Welcome } from '@storybook/react/demo';
 import Checker from '../src/components/SandBox.jsx';
 import CheckerIcon from '../src/components/Checker.jsx';
-import CheckerIconSvg from '../src/checker.svg';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
@@ -20,6 +19,6 @@ storiesOf('SandBox', module)
 
 
 storiesOf('Checker', module)
-  .add('red', () => <CheckerIcon color='red'></CheckerIcon>)
-  .add('yellow', () => <CheckerIcon color='yellow'></CheckerIcon>)
-  .add('svg icon', () => <div><img src={CheckerIconSvg} alt=""/></div>)
+  .add('red', () => <CheckerIcon color='red'/>)
+  .add('yellow', () => <CheckerIcon color='yellow'/>)
+  .add('200px', () => <CheckerIcon color='red' size='100' />)
